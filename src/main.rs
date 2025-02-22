4b2777e1496255bfe9786aa82e1c238cab1a9c26
Anish Karandikar <anishkny@gmail.com>
2025-02-21 16:51:09 -0800

    Add support for ignoring files with glob patterns

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use globset::{Glob, GlobSetBuilder};
 use ignore::WalkBuilder;
 use std::fs;
 use std::path::Path;
@@ -19,20 +20,52 @@ struct Args {
     help = "Include hidden files in the output"
   )]
   include_hidden: bool,
+
+  #[arg(
+    long = "ignore",
+    help = "Glob patterns of files or directories to ignore",
+    action = clap::ArgAction::Append
+  )]
+  ignore_patterns: Vec<String>,
+}
+
+struct ProcessPathOptions {
+  include_hidden: bool,
+  ignore_patterns: Vec<String>,
 }
 
 fn main() {
   let args = Args::parse();
+  let process_path_options = ProcessPathOptions {
+    include_hidden: args.include_hidden,
+    ignore_patterns: args.ignore_patterns.clone(),
+  };
   for path in &args.paths {
-    process_path(Path::new(path), args.include_hidden);
+    process_path(Path::new(path), &process_path_options);
   }
 }
 
-fn process_path(path: &Path, include_hidden: bool) {
-  let walker = WalkBuilder::new(path)
-    .hidden(!include_hidden)
+fn process_path(path: &Path, options: &ProcessPathOptions) {
+  let ProcessPathOptions {
+    include_hidden,
+    ignore_patterns,
+  } = options;
+  let mut walker = WalkBuilder::new(path);
+  walker.hidden(!include_hidden);
+
+  let mut glob_builder = GlobSetBuilder::new();
+  for pattern in ignore_patterns {
+    if let Ok(glob) = Glob::new(pattern) {
+      glob_builder.add(glob);
+    }
+  }
+  let glob_set = glob_builder.build().unwrap();
+
+  let walker = walker
+    .filter_entry(move |entry| !glob_set.is_match(entry.path()))
     .sort_by_file_path(|a, b| a.cmp(b))
     .build();
+
   for result in walker {
     match result {
       Ok(entry) => {
--- tests/cli-tests.rs
+++ tests/cli-tests.rs
@@ -22,12 +22,13 @@ const TARGET: &str = if cfg!(debug_assertions) {
 #[test_case(&["tests/inputs/04_invalid_utf8"], "04_invalid_utf8.golden.txt"; "04_invalid_utf8")]
 #[test_case(&["tests/inputs/05_hidden"], "05_hidden.golden.txt"; "05_hidden")]
 #[test_case(&["tests/inputs/05_hidden", "--include-hidden"], "05a_include_hidden.golden.txt"; "05a_include_hidden")]
+#[test_case(&["tests/inputs/06_ignore", "--ignore", "*.md", "--ignore", "*.csv"], "06_ignore.golden.txt"; "06_ignore")]
 fn test_files_to_prompt(input: &[&str], golden_filename: &str) {
   // Rename gitignore files to .gitignore if they exist
   let _gitignore_renamers = rename_gitignore_files(input);
 
   // Run the CLI tool
-  println!("Running CLI tool with input: {:?}", input);
+  println!("Running TARGET [{:}] with input: {:?}", TARGET, input);
   let output = Command::new(TARGET)
     .args(input.iter())
     .output()
